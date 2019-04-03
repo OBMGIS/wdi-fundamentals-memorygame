e56102ea83a385529e690d827b842ab9dd9181f9
orlando <obmgis@gmail.com>
2019-04-03 16:51:29 -0500

    kingandqueen

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("up and running");
+
+
+var cardone = "queen"
+var cardtwo = "queen"
+var cardthree = "king"
+var cardfour = "king"
+
+console.log("User Flipped " + cardthree);
+
+console.log("User Flipped " + cardone);
