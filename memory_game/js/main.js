b579d4dc4124c59c299eb9dc93fa2f66db94ada2
Orlando J. Batista-Morales <36862549+OBMGIS@users.noreply.github.com>
2019-04-06 18:06:53 -0500

    js hard

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,29 @@
-console.log("up and running");
 
 
-var cardone = "queen"
-var cardtwo = "queen"
-var cardthree = "king"
-var cardfour = "king"
 
-console.log("User Flipped " + cardthree);
+var cards = [ "queen", "queen", "king", "king"];
+
+var cardsinplay = [];
+
+var cardone = cards[0];
+
+cardsinplay.push(cardone);
 
 console.log("User Flipped " + cardone);
+
+var cardtwo = cards[3];
+
+cardsinplay.push(cardtwo);
+
+
+console.log("User Flipped " + cardtwo);
+
+
+
+if ( cardsinplay.length === 2) {
+
+if (cardsinplay[0] === cardsinplay[3] ) {
+	alert("you found a match!");
+} else 
+   {alert("sorry try again")};
+}
