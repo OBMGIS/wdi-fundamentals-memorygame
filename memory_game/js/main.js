b621f525a1ec737a0af57d1e40da1dba2cc1888c
Orlando J. Batista-Morales <36862549+OBMGIS@users.noreply.github.com>
2019-04-06 20:00:39 -0500

    latest

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -5,25 +5,22 @@ var cards = [ "queen", "queen", "king", "king"];
 
 var cardsinplay = [];
 
-var cardone = cards[0];
-
-cardsinplay.push(cardone);
-
-console.log("User Flipped " + cardone);
-
-var cardtwo = cards[3];
-
-cardsinplay.push(cardtwo);
-
-
-console.log("User Flipped " + cardtwo);
-
-
-
-if ( cardsinplay.length === 2) {
-
+var checkformatch = function(){
+		if ( cardsinplay.length === 2) {
 if (cardsinplay[0] === cardsinplay[3] ) {
 	alert("you found a match!");
 } else 
    {alert("sorry try again")};
+}  
 }
+
+var cardflip = function(cardid){
+console.log("user flipped" + cards[cardid]);
+cardsinplay.push(cards[cardid]);
+checkformatch();
+}  
+
+cardflip(0);
+
+cardflip(2);
+
